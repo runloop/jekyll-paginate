8ce111e83dd61a81ac1a0283e0e72eeac4b158a5
Daniel Wood <daniel.wood@runloop.com>
2016-03-23 15:46:12 +0000

    Update pagination.rb

--- lib/jekyll-paginate/pagination.rb
+++ lib/jekyll-paginate/pagination.rb
@@ -38,7 +38,7 @@ module Jekyll
       #                   "previous_page" => <Number>,
       #                   "next_page" => <Number> }}
       def paginate(site, page)
-        all_posts = site.site_payload['site']['posts'].reject { |post| post['hidden'] }
+        all_posts = site.site_payload['site']['posts'].reverse.reject { |post| post['hidden'] }
         pages = Pager.calculate_pages(all_posts, site.config['paginate'].to_i)
         (1..pages).each do |num_page|
           pager = Pager.new(site, num_page, all_posts, pages)
